eafd1f3fb83bd2ab792081ea3c98948e482aa270
lffteclado <lffteclado@gmail.com>
2018-11-25 20:47:45 -0200

    Criacao do Menu Superior

--- src/components/MenuSuperior/MenuSuperior.js
+++ src/components/MenuSuperior/MenuSuperior.js
@@ -4,20 +4,20 @@ class MenuSuperior extends Component{
     render(){
         return(
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <Link to="/" className="navbar-brand">Aulas React</Link>
+            <a href='/' className='navbar-brand'>Aulas React</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                   <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                        <Link to="/" className="nav-link">Resumo</Link>
+                        
                     </li>
                     <li className="nav-item">
-                        <Link to="/consultas" className="nav-link">Consultas</Link>
+                       
                     </li>
                     <li className="nav-item">
-                        <Link to="/faturamento" className="nav-link">Faturamento</Link>
+                        
                     </li>
                   </ul>
                 </div>
